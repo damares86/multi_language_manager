1de1a5314f58ccc3166965273c5843963a6d5d97
damares86 <85158984+damares86@users.noreply.github.com>
2022-10-28 11:31:54 +0200

    Update index.php

--- it/index.php
+++ it/index.php
@@ -17,52 +17,5 @@ require "../mlm/redir.php";
 
 ?>
 
-<!DOCTYPE html>
-<html lang="it">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="damares86" />
-        <title>Home - Multi Language Manager by damare86</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="../assets/css/styles.css" rel="stylesheet" />
-    </head>
-    <body>
-        <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-info">
-            <div class="container">
-                <a class="navbar-brand" href="https://github.com/damares86">
-                    <img src="../assets/damares86.png">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="pagina_di_prova.php">Pagina di prova</a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Italiano</a>
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="../mlm/translate.php?lang=<?=$actual_lang?>&page=<?=$page_name?>&new_lang=en">Inglese</a></li>
-                                <li><a class="dropdown-item" href="../mlm/translate.php?lang=<?=$actual_lang?>&page=<?=$page_name?>&new_lang=es">Spagnolo</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Page content-->
-        <div class="container">
-            <div class="text-center mt-5">
-                <h1>Benvenuto nella homepage in italiano</h1>
-                <p class="lead">Scegli un'altra lingua dal menu per cambiare lingua e aggiornare il cookie</p>                          
-            </div>
-        </div>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="../assets/js/scripts.js"></script>
-    </body>
-</html>
+<!-- SAMPLE LINK (SECONDARY LANGUAGE IN WEBSITE SUBFOLDER) -->
+<a class="dropdown-item" href="../mlm/translate.php?lang=<?=$actual_lang?>&page=<?=$page_name?>&new_lang=en">Inglese</a>
